2978b6a450525ba1fb59f3e7cad6ef2bcf40f4d1
firefenix <kitotavrik.media@gmail.com>
2024-08-17 01:09:25 +0300

    Добавлен шаблон конфигурации. Не дописана инициализация конфига.

--- src/config/config.py
+++ src/config/config.py
@@ -7,6 +7,22 @@ LANG_ID = Language.asm
 from platformdirs import user_config_dir
 from src.utils import Singleton
 import yaml
+import os
+
+
+configPath = f"{user_config_dir(APP_NAME)}/config.yaml"
+
+def generateConfig():
+
+	"""
+	Генерирует конфигурационный файл.
+ 
+	"""
+	os.makedirs(os.path.dirname(configPath), exist_ok=True)
+	with open("config_template/config.py") as file:
+		if file in os.listdir(user_config_dir(APP_NAME)):
+			pass	
+	
 
 
 def getSignInData() -> tuple[str, str]:
@@ -20,7 +36,7 @@ def getSignInData() -> tuple[str, str]:
 
 class ConfigManager(metaclass=Singleton):
 	def __init__(self):
-		with open(f"{user_config_dir(APP_NAME)}/config.yaml", "r") as file:
+		with open(configPath, "r") as file:
 			config = yaml.safe_load(file)
 		self.name = config["name"]
 		self.homeUrl = config["home-url"]
