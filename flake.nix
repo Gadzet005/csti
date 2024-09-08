bec8418b78a5a472eb486ea33ab09bcfd87649dd
firefenix <kitotavrik.media@gmail.com>
2024-09-08 15:09:22 +0300

    Flakes refactoring.

--- flake.nix
+++ flake.nix
@@ -1,42 +1,48 @@
 {
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-		poetry2nix = {
-			url = "github:nix-community/poetry2nix";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-	outputs = { self, nixpkgs, poetry2nix }: let
-		supportedSystems = [
-			"x86_64-linux"
-			"x86_64-darwin"
-			"aarch64-linux"
-			"aarch64-darwin"
-		];
-		forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-		pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
-	
-	in
-	{
-		packages = forAllSystems (system: let
-			inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryApplication;
-		
-		in {
-			csti = mkPoetryApplication { projectDir = self; };
-			default = self.packages.${system}.csti;
-		});
+  outputs = { self, nixpkgs, poetry2nix, }:
+    let
+      supportedSystems =
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
 
-		devShells = forAllSystems (system: let
-			inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryEnv;
-		
-		in {
-			default = pkgs.${system}.mkShellNoCC {
-				packages = with pkgs.${system}; [
-					(mkPoetryEnv { projectDir = self; })
-					poetry
-				];
-			};
-		});
-	};
+    in {
+      packages = forAllSystems (system:
+        let
+          inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; })
+            mkPoetryApplication;
+
+        in {
+          csti = mkPoetryApplication { projectDir = self; };
+          default = self.packages.${system}.csti;
+        });
+
+      devShells = forAllSystems (system:
+        let
+          inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; })
+            mkPoetryEnv;
+
+        in {
+          default = pkgs.${system}.mkShellNoCC {
+            packages = with pkgs.${system}; [
+              (mkPoetryEnv { projectDir = self; })
+              poetry
+
+              nixfmt
+            ];
+
+						shellHook = ''
+							exec fish 
+						'';
+          };
+        });
+    };
 }
---
+++ nix/shell_templates/c/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Basic csti flake.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    csti.url = "github:Gadzet005/csti";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      supportedSystems =
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+
+    in {
+      devShells = forAllSystems (system: {
+        default = pkgs.${system}.mkShellNoCC {
+          packages = with pkgs.${system};
+            [
+              #	Добавьте необходимые вам пакеты(их названия можно найти на
+              # сайте https://mynixos.com).
+              gcc
+              libclang # clag-format
+            ] ++ [ inputs.csti.packages.${system}.default ];
+
+          # Вместо shell подставьте вашу оболочку командной строки и
+          # расскоментируйте строку.
+          shellHook = ''
+            # exec shell
+          '';
+        };
+      });
+    };
+}
